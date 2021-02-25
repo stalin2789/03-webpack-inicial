0b44063b1ae6bd5e9a488964dc4644d63eab403c
Stalin <st.metalicboy@gmail.com>
2021-02-25 17:41:22 -0500

    webpack

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,7 +1,9 @@
+import '../css/componentes.css';
+
 export const saludar = (nombre)=>{
-    console.log('Creando etiqueta h1');
+    console.log('Creando etiqueta h1 en el HTML: ');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre}`;
+    h1.innerText = `Hola, ${nombre}, como estas?`;
     document.body.append(h1);
 }
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+
+    mode: 'development',
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin()]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                },
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        })
+
+    ]
+}
